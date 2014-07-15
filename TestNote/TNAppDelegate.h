75bb189ff34cab8b9a523f6a572be6ebc1d7f45f
Owner <owner@owners-mbp.home>
2014-07-14 21:23:53 -0400

    Initial Commit

---
+++ TestNote/TNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TNAppDelegate.h
+//  TestNote
+//
+//  Created by Owner on 7/14/14.
+//  Copyright (c) 2014 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestNote
+//
+//  Created by Owner on 7/14/14.
+//  Copyright (c) 2014 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TNAppDelegate class]));
+    }
+}
